d09b362a20782528ce167ae9bbac7a30cf928d85
joryrod <joryrodriguez25@gmail.com>
2024-10-30 12:48:50 -0500

    feet: respuesta 4

--- 4. Imprimir un arreglo/ejercicio.js
+++ 4. Imprimir un arreglo/ejercicio.js
@@ -1,5 +1,9 @@
 // escribe tu respuesta acá
-
+function imprimirArreglo(...arreglo){
+    for(const elemento of arreglo){
+        console.log(elemento);
+    }
+}
 // código de prueba
 console.log(imprimirArreglo(1, "Hola", 2, "Mundo"))
 // 1
