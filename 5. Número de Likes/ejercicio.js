e4c50bedaf017620acc116b1bce5396823d393db
joryrod <joryrodriguez25@gmail.com>
2024-10-30 12:43:46 -0500

    feet: respuesta 2

--- 5. Número de Likes/ejercicio.js
+++ 5. Número de Likes/ejercicio.js
@@ -1,5 +1,13 @@
 // escribe tu respuesta acá
-
+function likes(num) {
+    if (num < 1000) {
+      return String(num);
+    } else if (num >= 1000 && num < 1000000) {
+      return (num / 1000 | 0) + "K";
+    } else {
+      return (num / 1000000 | 0) + "M";
+    }
+  }
 // código de prueba
 console.log(likes(983)) // "983"
 console.log(likes(1900)) // "1K"
