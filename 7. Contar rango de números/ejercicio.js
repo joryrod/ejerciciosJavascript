617861291b130aa38fa5003be100b39085f15e02
joryrod <joryrodriguez25@gmail.com>
2024-10-30 13:01:52 -0500

    feet: respuesta 7

--- 7. Contar rango de números/ejercicio.js
+++ 7. Contar rango de números/ejercicio.js
@@ -1,5 +1,13 @@
 // escribe tu respuesta acá
-
+function contarRango(num1, num2) {
+    let contador = 0;
+    let i = num1 + 1;
+    while (i < num2) {
+      contador++;
+      i++;
+    }
+    return contador;
+  }
 // código de prueba
 console.log(likes(983)) // "983"
 console.log(likes(1900)) // "1K"
