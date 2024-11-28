bd9cfe5921fd59a16006a934e0439ad7b5fe2a88
joryrod <joryrodriguez25@gmail.com>
2024-11-27 21:05:18 -0500

    feat:solucion del ejercicio 9

--- 9. Número de aes (letra a)/ejercicio.js
+++ 9. Número de aes (letra a)/ejercicio.js
@@ -1,16 +1,14 @@
 // escribe tu respuesta acá
-function contarRango(num1, num2) {
-    let contador = 0;
-    let i = num1 + 1;
-    while (i < num2) {
-      contador++;
-      i++;
-    }
-    return contador;
+function numeroDeAes(cadena) {
+  let contador = 0;
+  for (let i = 0; i < cadena.length; i++) {
+      if (cadena[i] === 'a') {
+          contador++;
+      }
   }
+  return contador;
+}
 // código de prueba
-console.log(likes(983)) // "983"
-console.log(likes(1900)) // "1K"
-console.log(likes(54000)) // "54K"
-console.log(likes(120800)) // "120K"
-console.log(likes(25222444)) // "25M"
+console.log(numeroDeAes("abracadabra")) // 5
+console.log(numeroDeAes("etinol")) // 0
+console.log(numeroDeAes("")) // 0
